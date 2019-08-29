7c574bf8f621d9e49d1d15a62c695a203f27ad72
fengcheng.zhang@percent.cn <fengcheng.zhang@percent.cn>
2019-08-29 17:53:20 +0800

    菜单栏的标题
    完成开发

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,62 +4,85 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    menu_items: [{
+      id: 1,
+      name: '知识点'
+    }, {
+      id: 2,
+      name: '难度'
+    }, {
+      id: 3,
+      name: '类型'
+    }],
+    select_menu_id:1,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
+
+  },
+  onMenuSelected:function(event){
+    console.log(event);
+    var selectedId = event.target.dataset.id;
+
+    if(selectedId==this.data.select_menu_id){
+      console.log('当前页面已经打开，无需再次处理')
+      return;
+    }
     
+    this.setData({
+      select_menu_id:selectedId
+    })
   }
 })
