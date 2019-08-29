96f57957b67aee09336d99a91cbba761e8e431d2
fengcheng.zhang@percent.cn <fengcheng.zhang@percent.cn>
2019-08-29 23:29:12 +0800

    增加可选选项

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,15 +6,52 @@ Page({
   data: {
     menu_items: [{
       id: 1,
-      name: '知识点'
+      name: '知识点',
+      levels: []
     }, {
       id: 2,
-      name: '难度'
+      name: '难度',
+      levels: [{
+          id: 1,
+          title: '不限'
+        },
+        {
+          id: 2,
+          title: '简单'
+        },
+        {
+          id: 3,
+          title: '中等'
+        },
+        {
+          id: 4,
+          title: '困难'
+        },
+      ]
     }, {
       id: 3,
-      name: '类型'
+      name: '类型',
+      levels: [{
+          id: 1,
+          title: '不限'
+        },
+        {
+          id: 2,
+          title: '选择题'
+        },
+        {
+          id: 3,
+          title: '填空题'
+        },
+        {
+          id: 4,
+          title: '解答题'
+        },
+      ]
     }],
-    select_menu_id:0,
+    select_menu_id: 0,
+    hard_select_id: 1,
+    type_select_id: 1
   },
 
   /**
@@ -72,23 +109,51 @@ Page({
   onShareAppMessage: function() {
 
   },
-  onMenuSelected:function(event){
+  onMenuSelected: function(event) {
     console.log(event);
     var selectedId = event.target.dataset.id;
 
-    if(selectedId==this.data.select_menu_id){
+    if (selectedId == this.data.select_menu_id) {
       console.log('当前页面已经打开，无需再次处理')
       return;
     }
-    
+
     this.setData({
-      select_menu_id:selectedId
+      select_menu_id: selectedId
     })
   },
-  doConfirm:function(){
+  doConfirm: function() {
     //TODO 点击确认后 应该对数据进行过滤
     this.setData({
       select_menu_id: 0
     })
+  },
+  onHardMenuSelected: function(event) {
+    console.log(event);
+    var selectedId = event.target.dataset.id;
+
+    if (selectedId == this.data.hard_select_id) {
+      console.log('当前页面已经打开，无需再次处理')
+      return;
+    }
+
+    this.setData({
+      hard_select_id: selectedId
+    })
+
+  },
+  onTypeMenuSelected: function(event) {
+    console.log(event);
+    var selectedId = event.target.dataset.id;
+
+    if (selectedId == this.data.type_select_id) {
+      console.log('当前页面已经打开，无需再次处理')
+      return;
+    }
+
+    this.setData({
+      type_select_id: selectedId
+    })
+
   }
 })
